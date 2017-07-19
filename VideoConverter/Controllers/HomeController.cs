89b985544939c122eb60fa442cc23c76222b2f38
mhsjaber <mhsjaber@gmail.com>
2017-07-19 20:23:49 +0600

    init

---
+++ VideoConverter/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using NReco.VideoConverter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VideoConverter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+            ffMpeg.ConvertMedia(@"C:\Users\Jaber\Videos\2017-05-20 12-14-27.flv", @"D:\video.mp4", Format.mp4);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ VideoConverter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VideoConverter.MvcApplication" Language="C#" %>
